c18bc3df799bce8417a7f2962e102f76ed1fd1b7
Divansir-Junior <jscrobut989@gmail.com>
2025-08-02 01:03:35 -0300

    (CREATE ) Function to submit dates created

--- src/script/main.js
+++ src/script/main.js
@@ -1,9 +1,19 @@
+
+//Manda a data para o fetch
+function sendDate() {
+    const btnSend = document.getElementById("btnSend").addEventListener("click",() => {
+        checkDate();
+    })
+}
+
 // Checa se é uma data válida
 function checkDate() {
     const date = document.getElementById("dateInput").value;
-    if(!date.value) {
+    if(!date) {
         alert("INVALID DATA");
     }
+    console.log("DATA"  + date.value);
+   
 }
 
-checkDate();
+sendDate();
