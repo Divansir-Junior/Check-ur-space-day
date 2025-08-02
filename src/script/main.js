7cfce645fa2883ab3cd2a462ca57562940454dda
Divansir-Junior <jscrobut989@gmail.com>
2025-08-02 01:21:52 -0300

    (CREATED) Function to fetch data from NASA API created

--- src/script/main.js
+++ src/script/main.js
@@ -1,8 +1,10 @@
+const imgApi = document.getElementById("imgApi");
 
 //Manda a data para o fetch
 function sendDate() {
     const btnSend = document.getElementById("btnSend").addEventListener("click",() => {
         checkDate();
+        fetchAPOD();
     })
 }
 
@@ -15,5 +17,26 @@ function checkDate() {
     console.log("DATA"  + date.value);
    
 }
+// Faz a conexão com a API da Nasa .
+async function fetchAPOD() {
+    const dateInput = document.getElementById("dateInput").value;
+    const PUBLIC_API_KEY = "IZDvfoMZnJxWaKw8GsBzNSXSdEZKsDWdVLGGDkjq";
+    const url = `https://api.nasa.gov/planetary/apod?api_key=${PUBLIC_API_KEY}&date=${dateInput}`;
+    
+    try {
+        const response = await fetch(url);
 
+        if(!response.ok) {
+            throw new Error(`ERROR: ${response.status}`);
+        }
+
+        const data = await response.json();
+
+        //Atuliza o conteúdo
+        imgApi.src = data.url;
+
+    } catch(error) {
+        console.log("Error to fetch data" + error);
+    }
+}
 sendDate();
